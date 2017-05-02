40b9398ba7b1df501a03b012439ff1a4ca167769
Sandeep1991 <sandeepvadiraj@yahoo.co.in>
2017-05-02 00:05:15 -0700

    Update nb_class.js

--- nb_class.js
+++ nb_class.js
@@ -4,14 +4,14 @@ var fs = require('fs');
 var junk = require('junk');
 var myArr=[]
 var allConc=fs.readdirSync('./concerns/');
-console.log(allConc.filter(junk.not));
+//console.log(allConc.filter(junk.not));
 //Initialize the dictionary to empty values
 var allF=allConc.filter(junk.not);
 var myDict={}
 allF.forEach(function(value){
 myDict[value]=[];
 });
-console.log(myDict);
+//console.log(myDict);
 
 for (var key in myDict)
 {
@@ -34,6 +34,18 @@ classifier.train();
 
 //console.log(classifier.classify(myDict['gui']));
 //console.log(classifier.getClassifications(myDict['gui']));
+var merge = require('merge'), original, cloned;
+
+var dict_nb={};
+var obj=classifier.getClassifications(myDict['gui']);
+var th_val=-1;
+obj.forEach(function(value)
+{
+ 	dict_nb[value['label']]=value['value'];
+ 	if(th_val<value['value'])
+ 		th_val=value['value'];
+ });
+//console.log(dict_nb);
 var allComp=fs.readdirSync('./test/');
 var compFil=allComp.filter(junk.not);
 var cnt=0;
@@ -42,5 +54,31 @@ compFil.forEach(function(value){
 	console.log('File ' + cnt.toString()+': '+value);
 	console.log(classifier.getClassifications(value));
 });
-
+//Algorithm
+var SPFSmells={};
+//var concernCounts=0;
+compFil.forEach(function(cFile){
+	classifier.getClassifications(cFile)
+	var dict_nb={};
+	var obj=classifier.getClassifications(cFile);
+	var th_val=-1;
+	obj.forEach(function(value)
+	{
+ 		//obj3=merge(obj3,value);
+ 		dict_nb[value['label']]=value['value'];
+ 		if(th_val<value['value'])
+ 			th_val=value['value'];
+ 	});
+ 	for(var key in dict_nb){
+ 		if(th_val===dict_nb[key] && !(key in SPFSmells))
+ 		{	
+ 		 	SPFSmells[key]=[];
+ 		 	SPFSmells[key].push(cFile);
+ 		 }
+ 		else if(th_val===dict_nb[key] && (key in SPFSmells))
+ 			SPFSmells[key].push(cFile);
+ 		}
+ });
+ console.log(SPFSmells);
+ 
 		
